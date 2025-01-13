19f55d9ced7f2c3d625bd143bdc3ffc858331a3d
Naeel <tazet@narod.ru>
2025-01-13 19:41:40 +0300

    memStor := MemStorage{}

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,6 +12,8 @@ import (
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/go-resty/resty/v2"
 )
 
 type gauge float64
@@ -124,11 +127,11 @@ func main() {
 }
 
 func run() error {
-	memStor := new(MemStorage)
+	memStor := MemStorage{} //memStor := new(MemStorage)
 	for {
 		cunt := 0
 		for i := 0; i < reportInterval/pollInterval; i++ {
-			err := getMetrix(memStor)
+			err := getMetrix(&memStor)
 			if err != nil {
 				log.Println(err, "getMetrix")
 			} else {
@@ -151,4 +154,72 @@ func run() error {
 			}
 		}
 	}
+}
+
+func postBunch(bunch []Metrics) error {
+	marshalledBunch, err := json.Marshal(bunch)
+	if err != nil {
+		return err
+	}
+	compressedBunch, err := pack2gzip(marshalledBunch)
+	if err != nil {
+		return err
+	}
+	httpc := resty.New() //
+	httpc.SetBaseURL("http://localhost:8080")
+
+	httpc.SetRetryCount(3)
+	httpc.SetRetryWaitTime(1 * time.Second)    // начальное время повтора
+	httpc.SetRetryMaxWaitTime(9 * time.Second) // 1+3+5
+	//tn := time.Now()                           // -------------
+	httpc.SetRetryAfter(func(client *resty.Client, resp *resty.Response) (time.Duration, error) {
+		rwt := client.RetryWaitTime
+		//	fmt.Printf("waittime \t%+v\t time %+v  count %d\n", rwt, time.Since(tn), client.RetryCount) // -------
+		client.SetRetryWaitTime(rwt + 2*time.Second)
+		//	tn = time.Now() // ----------------
+		return client.RetryWaitTime, nil
+	})
+
+	req := httpc.R().
+		SetHeader("Content-Encoding", "gzip").
+		SetBody(compressedBunch).
+		SetHeader("Accept-Encoding", "gzip")
+
+	_, err = req.
+		SetDoNotParseResponse(false).
+		Post("/updates/")
+
+		//	log.Printf("%+v\n", resp)
+
+	return err
+}
+
+func makeBunchOfMetrics(memStor *MemStorage) []Metrics {
+	metrArray := make([]Metrics, 0, len(memStor.gau)+len(memStor.count))
+
+	for metrName, metrValue := range memStor.count {
+		mval := int64(metrValue)
+		metr := Metrics{ID: metrName, MType: "counter", Delta: &mval}
+		metrArray = append(metrArray, metr)
+	}
+	for metrName, metrValue := range memStor.gau {
+		mval := float64(metrValue)
+		metr := Metrics{ID: metrName, MType: "gauge", Value: &mval}
+		metrArray = append(metrArray, metr)
+	}
+	return metrArray
+}
+
+func pack2gzip(data2pack []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	//	zw.ModTime = time.Now()
+	_, err := zw.Write(data2pack)
+	if err != nil {
+		return nil, fmt.Errorf("gzip.NewWriter.Write %w ", err)
+	}
+	if err := zw.Close(); err != nil {
+		return nil, fmt.Errorf("gzip.NewWriter.Close %w ", err)
+	}
+	return buf.Bytes(), nil
 }
