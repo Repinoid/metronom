6c3a48832579a4dfcfc91695a8d64df9b3783c0a
Tatare <tazet@narod.ru>
2025-01-20 17:52:06 +0300

    hz wtf

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -38,8 +38,10 @@ var host = "localhost:8080"
 var sugar zap.SugaredLogger
 
 var ctx context.Context
-var memStor *memos.MemoryStorageStruct // 	in memory Storage
 var dbStorage *basis.DBstruct          // 	Data Base Storage
+
+var memStor *memos.MemoryStorageStruct // 	in memory Storage
+
 var inter models.Inter                 // 	= memStor OR dbStorage
 
 func main() {
