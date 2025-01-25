9562a83f15c7788081ef8b7fe46598b4a7e7799d
Tatare <tazet@narod.ru>
2025-01-25 10:20:34 +0300

    no crypto

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -69,7 +69,7 @@ func run() error {
 	router.Use(middlas.GzipHandleEncoder)
 	router.Use(middlas.GzipHandleDecoder)
 	router.Use(middlas.WithLogging)
-	router.Use(CryptoHandleDecoder)
+	//	router.Use(CryptoHandleDecoder)
 
 	return http.ListenAndServe(host, router)
 }
