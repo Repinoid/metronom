7498fc184981a32aaf76fc5bd8b3737997d969af
Naeel <tazet@narod.ru>
2025-04-25 07:28:56 +0400

    added signal raw

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,10 +2,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/mux"
 
@@ -52,7 +56,7 @@ func main() {
 }
 
 // run. ЗАпуск сервера и хендлеры
-func Run() error {
+func Run() (err error) {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", handlera.PutMetric).Methods("POST")
@@ -64,7 +68,6 @@ func Run() error {
 	router.HandleFunc("/", handlera.BadPost).Methods("POST") // if POST with wrong arguments structure
 	router.HandleFunc("/ping", handlera.DBPinger).Methods("GET")
 
-
 	router.Use(middlas.GzipHandleEncoder)
 	router.Use(middlas.GzipHandleDecoder)
 	//router.Use(middlas.NoSugarLogging)	// или NoSugarLogging - или WithLogging ZAP логирование
@@ -73,6 +76,46 @@ func Run() error {
 
 	router.PathPrefix("/debug/").Handler(http.DefaultServeMux)
 
-	return http.ListenAndServe(Host, router)
-}
+	//servak := http.ListenAndServe(Host, router)
 
+	var srv = http.Server{Addr: Host, Handler: router}
+
+	idleConnsClosed := make(chan struct{})
+	// канал для перенаправления прерываний
+	// поскольку нужно отловить всего одно прерывание,
+	// ёмкости 1 для канала будет достаточно
+	sigint := make(chan os.Signal, 1)
+	// регистрируем перенаправление прерываний
+	signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// запускаем горутину обработки пойманных прерываний
+	go func() {
+		// читаем из канала прерываний
+		// поскольку нужно прочитать только одно прерывание,
+		// можно обойтись без цикла
+		<-sigint
+		// получили сигнал os.Interrupt, запускаем процедуру graceful shutdown
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// ошибки закрытия Listener
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		// сообщаем основному потоку,
+		// что все сетевые соединения обработаны и закрыты
+		close(idleConnsClosed)
+	}()
+	if err = srv.ListenAndServe(); err != http.ErrServerClosed {
+		// ошибки старта или остановки Listener
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+	// ждём завершения процедуры graceful shutdown
+	<-idleConnsClosed
+
+	// получили оповещение о завершении
+	// здесь можно освобождать ресурсы перед выходом,
+	// например закрыть соединение с базой данных,
+	// закрыть открытые файлы
+	
+	fmt.Println("Server Shutdown gracefully")
+
+	return err
+}
